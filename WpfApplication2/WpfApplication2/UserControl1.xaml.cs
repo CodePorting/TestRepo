510f89fda1f66ef469c90a718c3416a41f3bb140
CodePorting <mohsan.raza@codeporting.com>
2012-10-10 10:50:56 +0500

    First commit

---
+++ WpfApplication2/WpfApplication2/UserControl1.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    enum ProgressType
+    {
+        audioCDControl = 0,
+        videoCDControl = 1,
+        dataCDControl = 2,
+        audioDVDControl = 3,
+        videoDVDControl = 4,
+        dataDVDControl = 5
+    };
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        double stepSize;
+        ProgressType cType;
+        
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public double UserControlProgressLimit
+        {
+            get { return this.rectTotal.Width; }
+            set { 
+                    this.rectTotal.Width = value;
+                    this.stepSize = (int)this.rectTotal.Width / 100;
+                }
+        }
+
+        public double UserControlProgressValue
+        {
+            get { return this.rectProgress.Width; }
+            set {
+                    double val = 100.0 / 280.0;
+                    this.rectProgress.Width = val*value+25.0;
+                    this.DisplayString();
+                }
+        }
+
+        public double UserControlStepSize
+        {
+            get { return this.stepSize; }
+            set { this.stepSize = value; }
+        }
+
+        public void IncrementProgress(int steps)
+        {
+            if (steps == 0)
+                this.rectProgress.Width += stepSize;
+            else
+                this.rectProgress.Width += (steps * stepSize);
+        }
+
+
+        public void setControlType(int iType)
+        {
+            cType = (ProgressType)iType;
+            this.DisplayString();
+        }
+        
+
+        void DisplayString()
+        {
+            switch (cType)
+            {
+                case ProgressType.audioCDControl:
+                    //TextValue.Text = "0min                20min                 40min                 80min                 100min                 120min";
+                    //TextValue.Text = " 0min     10min     20min     30min    40min    50min    60min    70min   80min   90min   100min   110min    120min";
+                    TextValue.Text = " 0min    10min    20min     30min    40min    50min    60min    70min  80min  90min 100min 110min 120min";
+                    break;
+                case ProgressType.videoCDControl:
+                case ProgressType.dataCDControl:
+                    //TextValue.Text = "0 Mb        100 Mb       200 Mb      300 Mb      400 Mb      500 Mb      600 Mb      700 Mb";
+                    TextValue.Text = " 0Mb   100Mb    200Mb    300Mb   400Mb   500Mb   600Mb   700Mb";
+                    break;
+                case ProgressType.audioDVDControl:
+                case ProgressType.videoDVDControl:
+                case ProgressType.dataDVDControl:
+                    TextValue.Text = "0 Gb       1 Gb        2Gb        3 Gb       4 Gb       5 Gb       6 Gb      7 Gb     8 Gb";
+                    break;
+               
+            }
+            //if (this.rectTotal.Width <= 300)
+            //{
+            //    //TextValue.Text = "0 Mb        100 Mb       200 Mb      300 Mb      400 Mb      500 Mb      600 Mb      700 Mb";
+            //}
+            //else
+            //{
+            //    //TextValue.Text = "0 Gb          1 Gb            2Gb           3 Gb           4 Gb           5 Gb          6 Gb           7 Gb          8 Gb";
+            //}
+        }
+
+        private void TextValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void userControlProgress_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApplication2/WpfApplication2/Window1.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace WpfApplication2
+{
+    //enum ProgressType
+    //{
+    //    audioCDControl = 0,
+    //    videoCDControl = 1,
+    //    dataCDControl = 2,
+    //    audioDVDControl = 3,
+    //    videoDVDControl = 4,
+    //    dataDVDControl = 5
+    //};
+
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public static UserControl1 userControlP;
+        public static void StaticMethod()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                userControlP.UserControlProgressValue = i * 20;
+                Thread.Sleep(500);
+            }
+        }
+
+        public Window1()
+        {
+            InitializeComponent();
+
+            // for DVD 8 GB
+            userControlProgress.UserControlProgressLimit = 450;
+            userControlProgress.UserControlProgressValue = 15;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.setControlType(2);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.setControlType(0);
+            //Timer timer = new Timer(new TimerCallback(CheckStatus), null, 0, 2000);           
+
+            //userControlP = userControlProgress;
+            //Thread progThread = new Thread(new ThreadStart(StaticMethod));
+
+            //// Start the thread.
+            //progThread.Start();
+        }
+
+        void CheckStatus(Object state)
+        {
+            userControlProgress.UserControlProgressValue = 200;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.setControlType(5);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.setControlType(1);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.setControlType(2);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.UserControlProgressValue = 0;
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.UserControlProgressValue = 100;
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.UserControlProgressValue = 200;
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.UserControlProgressValue = 300;
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.UserControlProgressValue = 400;
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.UserControlProgressValue = 500;
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.UserControlProgressValue = 600;
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+            userControlProgress.UserControlProgressValue = 700;
+        }
+    }
+}
